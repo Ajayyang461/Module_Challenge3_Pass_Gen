a98dedcb34d1dbe206be1a94d5ebf3b633f317ab
A-Jay Yang <ajay_yang1995@live.com>
2022-06-26 18:40:25 -0500

    added varable and Arryays

--- script.js
+++ script.js
@@ -1,16 +1,103 @@
-// Assignment code here
+//GIVEN I need a new, secure password
+//WHEN I click the button to generate a password
+//THEN I am presented with a series of prompts for password criteria
+//WHEN prompted for password criteria
+//THEN I select which criteria to include in the password
+//WHEN prompted for the length of the password
+//THEN I choose a length of at least 8 characters and no more than 128 characters
+//WHEN asked for character types to include in the password
+//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+//WHEN I answer each prompt
+//THEN my input should be validated and at least one character type should be selected
+//WHEN all prompts are answered
+//THEN a password is generated that matches the selected criteria
+//WHEN the password is generated
+//THEN the password is either displayed in an alert or written to the page
 
+// Declared variable that holds an array of values uppercase.
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+// Declared variable that holds an array of values lowercase.
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+// Declared variable that holds an array of values special characters.
+var spChar = ["!", "#", "$", "%", "&", "*", "+", "<", "=", ">", "?", "@"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  //prompt for 8 to 128 character long.
   var password = generatePassword();
+  var length = prompt("Your password needs to be at 8 to 128 charachter long.");
+
+  //prompt for uppercase letters
+  var upCase = confirm(
+    "Click ok to inlcude an uppercase letter to your password."
+  );
+  //prompt for lowercase letters
+  var lowCase = confirm(
+    "Click ok to inlude an lowercase letter your password."
+  );
+  //prompt for special character letters
+  var spChar = confirm("Click ok to include an special character.");
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
