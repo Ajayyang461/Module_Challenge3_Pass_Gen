9bbaa1d1b754512b1f085a3f85e11088c26f7431
A-Jay Yang <ajay_yang1995@live.com>
2022-06-27 20:55:40 -0500

    add a false check and for loop

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-//GIVEN I need a new, secure password
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-//WHEN asked for character types to include in the password
-//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
+// Get references to the #generate element.
+var generateBtn = document.querySelector("#generate");
 
-// Declared variable that holds an array of values uppercase.
 var upperCase = [
   "A",
   "B",
@@ -75,30 +61,89 @@ var lowerCase = [
 // Declared variable that holds an array of values special characters.
 var spChar = ["!", "#", "$", "%", "&", "*", "+", "<", "=", ">", "?", "@"];
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var numChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+function generatePassword() {
+  var passSelector = "";
+  var passResulst = "";
+
+  //prompt for 8 to 128 character long.
+  var lengthSelector = prompt(
+    "Your password needs to be at 8 to 128 charachter long."
+  );
+  // if you dont enter a number you will be prompt that it is required.
+  if (Number.isNaN(lengthSelector)) {
+    alert("It is required to input a number for the password length");
+    return passResulst;
+  }
+  //if the number is less than 8 you will propmpt an error message.
+  if (lengthSelector < 8 || lengthSelector > 128) {
+    alert("Your password cannot be less than 8 character long");
+    return passResulst;
+  }
+  // if the number is more than 128 you will be prompt an error message.
+  if (lengthSelector > 128) {
+    alert("Your password cannont be more than 128 characther long");
+    return passResulst;
+  }
+  //prompt for uppercase letters.
+  var needUpCase = confirm(
+    "Click ok to inlcude an uppercase letter to your password."
+  );
+  //prompt for lowercase letters.
+  var needLowCase = confirm(
+    "Click ok to inlude an lowercase letter to your password."
+  );
+  //prompt for special character letters.
+  var needSpChar = confirm(
+    "Click ok to include an special character to your password."
+  );
+  //prompt for numeric numbers.
+  var needNumChar = confirm(
+    "Click ok to inlude an numeric number to your password"
+  );
+  //checking the variable to see if it is false.
+  if (needLowCase === false) {
+    alert("Must select at least one character type");
+  }
+  if (needUpCase === false) {
+    alert("Must select at least one character type");
+  }
+  if (needSpChar === false) {
+    alert("Must select at least one character type");
+  }
+  if (needNumChar === false) {
+    alert("Must select at least one character type");
+  }
+  if (needLowCase) {
+    passSelector = passSelector.concat(lowerCase);
+  }
+  if (needUpCase) {
+    passSelector = passSelector.concat(upperCase);
+  }
+  if (needSpChar) {
+    passSelector = passSelector.concat(spChar);
+  }
+  if (needNumChar) {
+    passSelector = passSelector.concat(numChar);
+  }
+  for (var i = 0; i < lengthSelector; ++i) {
+    var randomSelector = passSelector.charAt(
+      Math.floor(Math.random() * passSelector.length)
+    );
+    passResulst = passResulst.concat(randomSelector);
+  }
+
+  return passResulst;
+}
 
 // Write password to the #password input
 function writePassword() {
-  //prompt for 8 to 128 character long.
   var password = generatePassword();
-  var length = prompt("Your password needs to be at 8 to 128 charachter long.");
-
-  //prompt for uppercase letters
-  var upCase = confirm(
-    "Click ok to inlcude an uppercase letter to your password."
-  );
-  //prompt for lowercase letters
-  var lowCase = confirm(
-    "Click ok to inlude an lowercase letter your password."
-  );
-  //prompt for special character letters
-  var spChar = confirm("Click ok to include an special character.");
-
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); // once click it will generate
